7bb30ce4eddadd15ee9efa0754f19b474e365a49
Loo Rong Jie <loorongjie@gmail.com>
2026-05-03 11:14:38 +0800

    Default 800px width

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,10 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    var frame = self.frame
+    frame.size = NSSize(width: 800, height: 800)
+    self.setFrame(frame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
